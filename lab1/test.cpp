8791d9ed802c0548a665b422afe975b7a53e57cb
Mihail Zyryanov <113300584+belqee@users.noreply.github.com>
2023-09-30 16:08:51 +0700

    Update test.cpp

--- lab1/test.cpp
+++ lab1/test.cpp
@@ -15,18 +15,17 @@ TEST(class_word_counter, correct_out) {
     in << "abc abc ab abc ab a a";
     counter.count_words(in);
     counter.write_result(out);
-
     EXPECT_EQ(out.str(), "abc;3;42%\na;2;28%\nab;2;28%\n");
-    //test 2
+    
     in.clear();
     out.clear();
     in.str("");
     out.str("");
-
+    
+    //second test
     in << "Better late ''' than ..,, never, ever never";
     counter.count_words(in);
     counter.write_result(out);
-
     EXPECT_EQ(out.str(), "never;2;33%\nBetter;1;16%\never;1;16%\nlate;1;16%\nthan;1;16%\n");
 }
 
